0a335ebfb0e69b682c54dac9fb07170960a56a68
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-07 14:28:36 +0100

    clock

---
+++ Fireworks/clock.js
@@ -0,0 +1,15 @@
+// ? timer
+const _clockDiv = document.querySelector("#clock");
+const _deadline =
+  "Wed Dec 02 2020 13:25:00 GMT+0100 (czas środkowoeuropejski standardowy)";
+
+function _getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  _clockDiv.innerHTML = `Time Left ${minutes < 10 ? "0" + minutes : minutes}:${
+    seconds < 10 ? "0" + seconds : seconds
+  }`;
+}
+
+setInterval(() => _getTimeRemaining(_deadline), 1000);
--- Fireworks/ready/app.js
+++ Fireworks/ready/app.js
@@ -1,18 +1,3 @@
-// ? timer
-const clockDiv = document.querySelector("#clock");
-const deadline =
-  "Wed Dec 02 2020 13:25:00 GMT+0100 (czas środkowoeuropejski standardowy)";
-
-function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  _clockDiv.innerHTML = `Time Left ${minutes < 10 ? "0" + minutes : minutes}:${
-    seconds < 10 ? "0" + seconds : seconds
-  }`;
-}
-
-setInterval(() => _getTimeRemaining(_deadline), 1000);
 
 // ? Definiowanie Zmiennych Globalnych
 const _canvas = document.querySelector("#_canvas");
