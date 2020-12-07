bab7784d409f17ee2c0e44b257a03c09cb74ed3d
kubo550 <43968748+kubo550@users.noreply.github.com>
2020-12-08 00:03:36 +0100

    Update sketch

--- Particles/ready/sektch.js
+++ Particles/ready/sektch.js
@@ -1,7 +1,16 @@
+let particles = [];
+
 function setup() {
   select("#_canvasPlace").child(createCanvas(470, 500));
+  particles = Array(30)
+    .fill(null)
+    .map(() => new Particle_());
 }
 
 function draw() {
-  background(31);
+  background("#ade8f4");
+  particles.forEach((p, i) => {
+    p.update();
+    p.drawLine(particles.slice(i));
+  });
 }
